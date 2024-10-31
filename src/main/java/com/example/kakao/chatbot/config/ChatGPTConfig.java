b667494245ff285900b80dbd957b87f8beb0e205
dalcheonroadhead <wjsaos2081@gmail.com>
2024-10-31 21:34:40 +0900

    feat: complete ChatBotService

--- src/main/java/com/example/kakao/chatbot/config/ChatGPTConfig.java
+++ src/main/java/com/example/kakao/chatbot/config/ChatGPTConfig.java
@@ -2,11 +2,15 @@ package com.example.kakao.chatbot.config;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
@@ -19,7 +23,16 @@ public class ChatGPTConfig {
 
     @Bean
     public RestTemplate restTemplate(){
-        return new RestTemplate();
+        // 프록시 설정
+        HttpHost proxy = new HttpHost("krmp-proxy.9rum.cc",3128,"http");
+        // 프록시를 사용한 HttpClient 생성
+        CloseableHttpClient httpClient = HttpClients.custom()
+            .setProxy(proxy)
+            .build();
+        // HttpClient를 사용하여 RestTemplate의 요청 팩토리 설정
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+
+        return new RestTemplate(factory);
     }
 
     @Bean
